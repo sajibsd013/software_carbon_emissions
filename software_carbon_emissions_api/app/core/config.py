a0094c211e137b8a48d9966c5e3957aa60c16e75
Sajib Sutradhar <78471300+sajibsd013@users.noreply.github.com>
2026-05-31 14:31:02 +0600

    Update GITHUB_TOKEN default value in config
    Set default value for GITHUB_TOKEN to an empty string.

--- software_carbon_emissions_api/app/core/config.py
+++ software_carbon_emissions_api/app/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Software Carbon Emissions API"
     VERSION: str = "1.0.0"
-    GITHUB_TOKEN: str
+    GITHUB_TOKEN: str = ""
 
     # This tells Pydantic to look for a .env file at the root level
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
