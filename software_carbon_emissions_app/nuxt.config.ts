3968e44c61fb5e1acc6034ed7ed87368afd28c14
Sajib Sutradhar <78471300+sajibsd013@users.noreply.github.com>
2026-05-31 13:16:28 +0600

    Enable server-side rendering in nuxt.config.ts
    Enable server-side rendering by setting ssr to true.

--- software_carbon_emissions_app/nuxt.config.ts
+++ software_carbon_emissions_app/nuxt.config.ts
@@ -2,7 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  ssr: false,
   
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/css/globals.css'],
