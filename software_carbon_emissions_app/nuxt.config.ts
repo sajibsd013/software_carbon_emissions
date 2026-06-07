ceffe354596815f3df8c1b3701cd1503f5215713
Sajib Sutradhar <sajibsd013@gmail.com>
2026-06-07 21:20:11 +0600

    Update Dockerfile commands and modify Nuxt configuration for production

--- software_carbon_emissions_app/nuxt.config.ts
+++ software_carbon_emissions_app/nuxt.config.ts
@@ -1,8 +1,7 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  
+
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/css/globals.css'],
 
@@ -11,30 +10,6 @@ export default defineNuxtConfig({
   },
 
   nitro: {
-    preset: 'azure',
-    prerender: {
-      crawlLinks: true,
-      routes: ['/']
-    }
-  },
-
-  vite: {
-    optimizeDeps: {
-      include: [
-        '@vue/devtools-core',
-        '@vue/devtools-kit',
-        'chart.js'
-      ]
-    },
-    server: {
-      watch: {
-        usePolling: true,
-        interval: 1000
-      },
-      hmr: {
-        protocol: 'ws',
-        port: 3000
-      }
-    }
+    preset: 'node-server'
   }
 })
